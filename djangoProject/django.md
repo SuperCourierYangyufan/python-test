9ceb2b81c021b1dbfa2ab674ded56b43c930cb73
852287099@qq.com <852287099@qq.com>
2023-04-03 22:25:52 +0800

    第四天 django

---
+++ djangoProject/django.md
@@ -0,0 +1,3 @@
+# django
+### 创建django名称
+C:\Users\yyf\AppData\Local\Programs\Python\Python310\Scripts\django-admin.exe startproject 项目名称
---
+++ 常用内置库/datatime.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from datetime import datetime
+
+print(datetime.now())
+
+print(datetime(2022,3,4,21,12,42))
