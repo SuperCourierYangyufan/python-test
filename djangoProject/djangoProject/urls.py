d6c643ad510ea7e7c326f60c727091dccea4f89d
852287099@qq.com <852287099@qq.com>
2023-04-04 23:41:16 +0800

    第五天

--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app01 import  views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('index/',views.sayHello),
+    path('test01',views.test01),
+    path('test02', views.test02),
 ]
